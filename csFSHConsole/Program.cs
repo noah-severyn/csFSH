3bedad6d016301a83ea7aff190e9e1d98d95d0e7
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2023-04-16 20:28:45 -0400

    added test project

---
+++ csFSHConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csFSH;
+
+Console.WriteLine("Hello, World!");
+
+string folder = "C:\\Users\\Administrator\\Desktop\\fsh";
+Image<Rgba32> baseimg = Image.Load<Rgba32>(Path.Combine(folder, "7AB50E44-0986135E-E5040004-C0.bmp"));
+Image<Rgba32> alphaimg = Image.Load<Rgba32>(Path.Combine(folder, "7AB50E44-0986135E-E5040004-A0.bmp"));
+
+Image<Rgba32> output = FSHImage.Blend(alphaimg, baseimg);
+output.SaveAsPng(Path.Combine(folder, "output.png"));
